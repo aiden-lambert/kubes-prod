a59dac7cdfc65eec5a9722537ecab63789d7270e
Aiden Lambert <aiden.lambert@workingmouse.com.au>
2024-02-14 13:31:35 +1000

    fixingtypo in deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ docker push aidenlambert94/multi-client
 docker push aidenlambert94/multi-server
 docker push aidenlambert94/multi-worker
 # Apply all configs in the k8s folder
-kubectl apply -f k8s
+kubectl apply -f k8
 # Imperatively set latest images on each deployment
 kubectl set image deployments/server-deployment server=aidenlambert94/multi-server:$SHA
 kubectl set image deployments/client-deployment client=aidenlambert94/multi-client:$SHA
